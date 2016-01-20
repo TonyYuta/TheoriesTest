8ea2a4cb4466f7b9bacbbd80ad49cfd55a10d489
TonyYuta <tonykagaya@gmail.com>
2016-01-19 21:04:00 -0800

    TC fin

--- src/test/java/core/TheoryTest_002.java
+++ src/test/java/core/TheoryTest_002.java
@@ -3,7 +3,6 @@ package core;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Assume;
-import org.junit.Test;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
@@ -11,11 +10,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Theories.class)
 public class TheoryTest_002 {
-	int count = 0;
 	
 	@DataPoints
 	public static String[] s_array() {
-		return new String[] {"a", "b", "c", "d", "e", "f", "g"};
+		return new String[] {"a", "b", "c", "d", "e", "f"};
 	}
 	
 	@Theory
@@ -23,13 +21,7 @@ public class TheoryTest_002 {
 	
 	public void test_002_001(String s_01, String s_02, String s_03, String s_04, String s_05, String s_06) {
 		
-		System.out.print(s_01 + s_02 + s_03 + s_04 + s_05 + s_06 + " ");
-		Assume.assumeThat(s_01 + s_02 + s_03 + s_04 + s_05 + s_06, is("acbz"));
-		count++;
-	}
-	
-	@Test
-	public void counting() {
-		System.out.println("combinations:" + count);
+		System.out.println(s_01 + s_02 + s_03 + s_04 + s_05 + s_06 + " ");		
+		Assume.assumeThat(s_01 + s_02 + s_03 + s_04 + s_05 + s_06, is("acbamd"));
 	}
 }
